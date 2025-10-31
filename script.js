b2c917459216b9ecf0e36d4c2c1be04a27608ff8
Dedelene <voicudiana66@gmail.com>
2025-10-31 23:17:53 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById("btn");
+const tabel = document.getElementById("table");
+
+btn.addEventListener("click", () => {
+
+    const fname = document.getElementById("firstname");
+    const lname = document.getElementById("lastname");
+    const tel = document.getElementById("telephone");
+
+    if(tabel.rows.length === 0) {
+        const thead = document.createElement("thead");
+        thead.innerHTML = `
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Phone number</th>
+            </tr>
+        `;
+        tabel.appendChild(thead);
+    }
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `<td>${fname.value}</td><td>${lname.value}</td><td>${tel.value}</td>`;
+    tabel.appendChild(tr);
+
+    fname.value = "";
+    lname.value = "";
+    tel.value = "";
+})
